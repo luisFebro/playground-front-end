7c9264e29c42f1e5e158d80058ec6463fd28121e
Luis Febro <mr.febro@gmail.com>
2019-10-01 05:37:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+        <h1 className="main-title">PLAYGROUND - QUICK TESTING FRONT-END FUNCTIONALITIES</h1>
+        <p className="warning">*This app should be cleaned up after every end of testing. All workable pieces should be moved to a project or gallery folder</p>
+    </Fragment>
   );
 }
 
